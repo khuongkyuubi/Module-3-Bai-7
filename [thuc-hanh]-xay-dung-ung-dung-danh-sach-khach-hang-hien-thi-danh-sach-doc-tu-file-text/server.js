4d69f32814dcb292874d710d61a57b46fed6bfd3
khuongkyuubi <khuong.kyuubi@gmail.com>
2022-06-06 17:03:40 +0700

    add new practice

---
+++ [thuc-hanh]-xay-dung-ung-dung-danh-sach-khach-hang-hien-thi-danh-sach-doc-tu-file-text/server.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const fs = require('fs')
+const port = 8000;
+
+let server = http.createServer(function (req, res) {
+    // đọc dữ liệu từ file data.txt
+    let dataFile = '';
+    let html = '';
+    fs.readFile('./data/data.txt', 'utf8', function (err, str) {
+        dataFile = str.split(",")
+        dataFile.forEach((value, index) => {
+            html += '<tr>';
+            html += `<td>${index + 1}</td>`
+            html += `<td>${value}</td>`
+            html += `<td><button class="btn btn-danger">Delete</button></td>`
+            html += '</tr>';
+        });
+    });
+
+    fs.readFile('./templates/index.html', 'utf8', function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        data = data.replace('{list-user}', html)
+        res.write(data)
+        res.end()
+    });
+})
+
+server.listen(port, function () {
+    console.log('Serve running port ', port);
+})
